9f5a2ff5c9da56e864e4b9b7f9c80d88e37fe32b
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-06-06 22:43:38 +0300

    Small tidying up

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Board from './components/Board'
 import CurrentPlayer from './components/CurrentPlayer'
 
 function App() {
-  const [ board, setBoard ] = useState([[null, null, null],
-                                        [null, null, null],
-                                        [null, null, null]])
-  const [ currentPlayer, setCurrentPlayer ] = useState('X')    
+    const [ board, setBoard ] = useState([[null, null, null],
+                                          [null, null, null],
+                                          [null, null, null]])
+    const [ currentPlayer, setCurrentPlayer ] = useState('X')    
   
-  const changePlayer = () => currentPlayer === 'X' ? setCurrentPlayer('O') : setCurrentPlayer('X')
-  const makeAMove = (row, column) => {
-    console.log('MOVE', currentPlayer, row, column)
-    if (board[row][column] === null) {
-      const newBoard = [ ...board ]
-      console.log(newBoard)
-      newBoard[row][column] = currentPlayer
-      console.log(newBoard)
-      setBoard(newBoard)
-      changePlayer()  
-    } else {
-      console.log('INVALID MOVE')
+    const changePlayer = () => currentPlayer === 'X' ? setCurrentPlayer('O') : setCurrentPlayer('X')
+    const makeAMove = (row, column) => {
+        console.log('MOVE', currentPlayer, row, column)
+        if (board[row][column] === null) {
+            const newBoard = [ ...board ]
+            newBoard[row][column] = currentPlayer
+            setBoard(newBoard)
+            changePlayer()  
+        } else {
+            console.log('INVALID MOVE')
+        }
     }
-  }
 
-  return (
-    <div className="App">
-      <CurrentPlayer currentPlayer={currentPlayer} />
-      <Board board={board} handleClick={makeAMove} />
-    </div>
-  );
+return (
+        <div className="App">
+            <CurrentPlayer currentPlayer={currentPlayer} />
+            <Board board={board} handleClick={makeAMove} />
+        </div>
+    ); 
 }
 
 export default App;
